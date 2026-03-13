3700f71a04c7e3aa458b4ca7f6a76ca9e795cad6
codewithseth <codewithseth2026@gmail.com>
2026-03-13 11:05:06 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# khmerjava-api
---
+++ src/main/java/com/codewithseth/khmerjava_api/KhmerjavaApiApplication.java
@@ -0,0 +1,13 @@
+package com.codewithseth.khmerjava_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhmerjavaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KhmerjavaApiApplication.class, args);
+	}
+
+}
