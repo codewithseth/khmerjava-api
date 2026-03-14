ef233cfc91175825a4b579fcbc73041e64a0aefc
codewithseth <codewithseth2026@gmail.com>
2026-03-14 21:42:43 +0700

    add: implement SecurityConfig for HTTP security configuration

---
+++ src/main/java/com/codewithseth/khmerjava_api/security/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.codewithseth.khmerjava_api.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests((req) -> {
+            req.requestMatchers("/api/v1/auth/**").permitAll();
+            req.anyRequest().authenticated();
+        })
+        .formLogin(Customizer.withDefaults())
+        .httpBasic(Customizer.withDefaults())
+        .build();
+    }
+
+}
