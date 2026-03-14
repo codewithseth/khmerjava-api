6b634990aace4b641cd448cf2c6dcdfece4d3c07
codewithseth <codewithseth2026@gmail.com>
2026-03-14 17:09:42 +0700

    add: implement AuditorAware for JPA auditing support

--- src/main/java/com/codewithseth/khmerjava_api/KhmerjavaApiApplication.java
+++ src/main/java/com/codewithseth/khmerjava_api/KhmerjavaApiApplication.java
@@ -2,8 +2,10 @@ package com.codewithseth.khmerjava_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditorAwareImpl")
 public class KhmerjavaApiApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/codewithseth/khmerjava_api/config/AuditorAwareImpl.java
@@ -0,0 +1,16 @@
+package com.codewithseth.khmerjava_api.config;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component("auditorAwareImpl")
+public class AuditorAwareImpl implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of("Anonymous user");
+    }
+
+}
