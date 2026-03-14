1d5abe5c49a700819c9d12af4170327de826056a
codewithseth <codewithseth2026@gmail.com>
2026-03-14 17:13:52 +0700

    refactor: replace @Data with @Getter and @Setter in UserRequestDto

--- src/main/java/com/codewithseth/khmerjava_api/dto/UserRequestDto.java
+++ src/main/java/com/codewithseth/khmerjava_api/dto/UserRequestDto.java
@@ -1,8 +1,10 @@
 package com.codewithseth.khmerjava_api.dto;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 public class UserRequestDto {
     
     private String firstname;
