6d4eb9eb1b203f0c4efc39796bc0c7a9e5dd0512
codewithseth <codewithseth2026@gmail.com>
2026-03-15 22:43:09 +0700

    add: define user_roles table for many-to-many relationship between users and roles

--- schema.sql
+++ schema.sql
@@ -4,11 +4,27 @@ CREATE TABLE users (
     lastname VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     pwd_hash VARCHAR(100) NOT NULL,
-    roles VARCHAR(100) NOT NULL,
     bio TEXT,
     profile_pic VARCHAR(100),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     created_by VARCHAR(100) NOT NULL,
     updated_at TIMESTAMP,
     updated_by VARCHAR(100)
+);
+
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL UNIQUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    created_by VARCHAR(100) NOT NULL,
+    updated_at TIMESTAMP,
+    updated_by VARCHAR(100)
+);
+
+CREATE TABLE user_roles (
+    user_id INT NOT NULL,
+    role_id INT NOT NULL,
+    PRIMARY KEY (user_id, role_id),
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE
 );
