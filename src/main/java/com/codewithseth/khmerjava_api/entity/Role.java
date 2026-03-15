f087c30ea8313027c663c32ce8e0ee797648cb89
codewithseth <codewithseth2026@gmail.com>
2026-03-15 22:53:59 +0700

    add: create Role entity and establish many-to-many relationship with User

---
+++ src/main/java/com/codewithseth/khmerjava_api/entity/Role.java
@@ -0,0 +1,32 @@
+package com.codewithseth.khmerjava_api.entity;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "roles")
+public class Role extends BaseEntity {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name", nullable = false, length = 50, unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users = new LinkedHashSet<>();
+
+}
--- src/main/java/com/codewithseth/khmerjava_api/entity/User.java
+++ src/main/java/com/codewithseth/khmerjava_api/entity/User.java
@@ -1,10 +1,17 @@
 package com.codewithseth.khmerjava_api.entity;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -31,13 +38,18 @@ public class User extends BaseEntity {
     @Column(name = "pwd_hash", nullable = false, length = 100)    
     private String password;
 
-    @Column(name = "roles", nullable = false, length = 100)
-    private String roles;
-
     @Column(name = "bio", columnDefinition = "TEXT")
     private String bio;
 
     @Column(name = "profile_pic", length = 100)
     private String profilePicture;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+        name = "user_roles",
+        joinColumns = @JoinColumn(name = "user_id"),
+        inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new LinkedHashSet<>();
 
 }
