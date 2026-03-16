f38a207ab38a39feae88281e7cb0777fcef873ce
codewithseth <codewithseth2026@gmail.com>
2026-03-16 12:50:53 +0700

    add: create AppConstants interface for JWT configuration constants

---
+++ src/main/java/com/codewithseth/khmerjava_api/constant/AppConstants.java
@@ -0,0 +1,7 @@
+package com.codewithseth.khmerjava_api.constant;
+
+public interface AppConstants {
+    String JWT_HEADER = "Authorization";  
+    String JWT_SECRET_KEY = "JWT_SECRET";
+    String JWT_SECRET_DEFAULT_VALUE = "jxgEQeXHuPq8VdbyYFNkANdudQ53YUn4";
+}
