6838b90326860469704bd87e6e5377d6d0f3b84f
codewithseth <codewithseth2026@gmail.com>
2026-03-16 14:52:40 +0700

    add: populate initial user and role data in database

--- data.sql
+++ data.sql
@@ -1,12 +1,19 @@
 INSERT INTO users 
     (firstname, lastname, email, pwd_hash, bio, profile_pic, created_at, created_by, updated_at, updated_by)
 VALUES
-    ('John', 'Doe', 'john@example.com', 'hashedpassword1', 'Tech blogger', 'john.jpg', CURRENT_TIMESTAMP, 'DBA', NULL, NULL),
-    ('Jane', 'Smith', 'jane@example.com', 'hashedpassword2', 'Java developer', 'jane.jpg', CURRENT_TIMESTAMP, 'DBA', NULL, NULL),
-    ('David', 'Lee', 'david@example.com', 'hashedpassword3', 'Tech reader', 'david.jpg', CURRENT_TIMESTAMP, 'DBA', NULL, NULL);
+    ('Super', 'Admin', 'superadmin@example.com', '$2a$10$Z.AtJfB9puB/rz9RF41Qa.tYOazyNid6TBSelC.d4j/9I18Nz2NRS', 'System super admin', 'superadmin.jpg', CURRENT_TIMESTAMP, 'DBA', NULL, NULL),
+    ('Admin', 'User', 'admin@example.com', '$2a$10$Z.AtJfB9puB/rz9RF41Qa.tYOazyNid6TBSelC.d4j/9I18Nz2NRS', 'System administrator', 'admin.jpg', CURRENT_TIMESTAMP, 'DBA', NULL, NULL),
+    ('Normal', 'User', 'user@example.com', '$2a$10$Z.AtJfB9puB/rz9RF41Qa.tYOazyNid6TBSelC.d4j/9I18Nz2NRS', 'Regular system user', 'user.jpg', CURRENT_TIMESTAMP, 'DBA', NULL, NULL);
 
 INSERT INTO roles
     (name, created_at, created_by, updated_at, updated_by)
 VALUES
+    ('ROLE_SUPER_ADMIN', CURRENT_TIMESTAMP, 'DBA', NULL, NULL),
     ('ROLE_ADMIN', CURRENT_TIMESTAMP, 'DBA', NULL, NULL),
     ('ROLE_USER', CURRENT_TIMESTAMP, 'DBA', NULL, NULL);
+
+INSERT INTO user_roles (user_id, role_id)
+VALUES
+    (1, 1),
+    (2, 2),
+    (3, 3);
