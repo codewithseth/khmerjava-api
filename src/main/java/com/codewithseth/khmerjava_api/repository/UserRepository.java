c28e7182489afe4d3e9784dbaab3de86df6265bb
codewithseth <codewithseth2026@gmail.com>
2026-03-16 10:38:22 +0700

    add: implement findByEmail method in UserRepository for user lookup by email

--- src/main/java/com/codewithseth/khmerjava_api/repository/UserRepository.java
+++ src/main/java/com/codewithseth/khmerjava_api/repository/UserRepository.java
@@ -1,9 +1,10 @@
 package com.codewithseth.khmerjava_api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.codewithseth.khmerjava_api.entity.User;
+import java.util.Optional;
 
-@Repository
-public interface UserRepository extends JpaRepository<User, Integer> {}
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+}
