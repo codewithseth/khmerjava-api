315ed327108d02e2bf5441bacab40987fe649462
codewithseth <codewithseth2026@gmail.com>
2026-03-16 09:07:33 +0700

    add: update data.sql to insert user data and roles with correct fields

--- data.sql
+++ data.sql
@@ -1,6 +1,12 @@
 INSERT INTO users 
-    (firstname, lastname, email, pwd_hash, roles, bio, profile_pic, created_at, created_by, updated_at, updated_by)
+    (firstname, lastname, email, pwd_hash, bio, profile_pic, created_at, created_by, updated_at, updated_by)
 VALUES
-    ('John', 'Doe', 'john@example.com', 'hashedpassword1', 'ADMIN', 'Tech blogger', 'john.jpg', NOW(), 'Admin', NULL, NULL),
-    ('Jane', 'Smith', 'jane@example.com', 'hashedpassword2', 'AUTHOR', 'Java developer', 'jane.jpg', NOW(), 'Admin', NULL, NULL),
-    ('David', 'Lee', 'david@example.com', 'hashedpassword3', 'USER', 'Tech reader', 'david.jpg', NOW(), 'Admin', NULL, NULL);
+    ('John', 'Doe', 'john@example.com', 'hashedpassword1', 'Tech blogger', 'john.jpg', CURRENT_TIMESTAMP, 'DBA', NULL, NULL),
+    ('Jane', 'Smith', 'jane@example.com', 'hashedpassword2', 'Java developer', 'jane.jpg', CURRENT_TIMESTAMP, 'DBA', NULL, NULL),
+    ('David', 'Lee', 'david@example.com', 'hashedpassword3', 'Tech reader', 'david.jpg', CURRENT_TIMESTAMP, 'DBA', NULL, NULL);
+
+INSERT INTO roles
+    (name, created_at, created_by, updated_at, updated_by)
+VALUES
+    ('ROLE_ADMIN', CURRENT_TIMESTAMP, 'DBA', NULL, NULL),
+    ('ROLE_USER', CURRENT_TIMESTAMP, 'DBA', NULL, NULL);
